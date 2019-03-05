d86284bf00ca004bd93923439d818a749289ef3f
Kelsey Low <kklowlow@gmail.com>
2019-03-05 22:31:20 +0000

    first commit

---
+++ lib/climb_catalog.rb
@@ -0,0 +1,6 @@
+require "climb_catalog/version"
+
+module ClimbCatalog
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/climb_catalog/version.rb
@@ -0,0 +1,3 @@
+module ClimbCatalog
+  VERSION = "0.1.0"
+end
