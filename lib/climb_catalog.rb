b9394fe1b20b2d62a3efd81c0d17ee83a5f328aa
Kelsey Low <kklowlow@gmail.com>
2019-03-06 03:20:23 +0000

    stubbing out climb.rb

--- lib/climb_catalog.rb
+++ lib/climb_catalog.rb
@@ -1,6 +1,6 @@
 require_relative "./climb_catalog/version.rb"
 require_relative "./climb_catalog/cli.rb"
-
+require_relative "./climb_catalog/climb.rb"
 
 
 # module ClimbCatalog
--- lib/climb_catalog/climb.rb
+++ lib/climb_catalog/climb.rb
@@ -1,15 +1,29 @@
 class ClimbCatalog::Climb 
+  attr_accessor :name, :grade, :location, :url
   
   def self.boulder
-    # @boulder = ClimbCatalog::Climb.boulder #=> lists bouldering routes
+    # @boulder = ClimbCatalog::Climb.boulder #=> return instances of bouldering routes
+    boulder_1 = self.new
+    boulder_1.name = "Climb name"
+    boulder_1.grade = "Climb grade"
+    boulder_1.location = "Climb location"
+    boulder_1.url = "Climb url"
+    
+    boulder_2 = self.new
+    boulder_2.name = "Climb name"
+    boulder_2.grade = "Climb grade"
+    boulder_2.location = "Climb location"
+    boulder_2.url = "Climb url"
+    
+    [boulder_1, boulder_2]
   end
   
   def self.sport
-    # @sport = ClimbCatalog::Climb.sport #=> lists sport climbing routes
+    # @sport = ClimbCatalog::Climb.sport #=> return instances of sport climbing routes
   end
   
   def self.trad
-    # @trad = ClimbCatalog::Climb.trad #=> lists trad climbing routes
+    # @trad = ClimbCatalog::Climb.trad #=> return instances of trad climbing routes
   end
     
   def grade
