57cdf28db57a72fc0e20f7efc1664e8496b22444
Kelsey Low <kklowlow@gmail.com>
2019-03-06 02:17:35 +0000

    stubbing out cli.rb

--- lib/climb_catalog/cli.rb
+++ lib/climb_catalog/cli.rb
@@ -1,11 +1,37 @@
 class ClimbCatalog::CLI
   
   def call
-    puts "Climbing?"
     list_categories
+    menu
+    goodbye
   end
   
   def list_categories
-    puts "Bouldering, sport climbing, or traditional climbing?"
+    puts "Welcome, climber!"
+    # pull category from mountain project for user to decide; input by typing category or 1, 2, or 3?
+    puts "Boulder"
+    puts "Sport"
+    puts "Trad"
   end
+  
+  def menu
+    puts "What type of climb would like today? Type exit to leave."
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "boulder"
+        puts "Lists boulders..."
+      when "sport"
+       puts "Lists sport climbs..."
+      when "trad"
+        puts "Lists trad climbs..."
+      end
+    end
+  end
+  
+  def goodbye
+    puts "Climb on!"
+  end
+  
 end
