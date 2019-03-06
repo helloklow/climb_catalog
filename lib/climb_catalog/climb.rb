386cfb88d7b3ad2d10d28ece5f743a866a95073c
Kelsey Low <kklowlow@gmail.com>
2019-03-06 03:07:33 +0000

    added climb.rb

---
+++ lib/climb_catalog/climb.rb
@@ -0,0 +1,23 @@
+class ClimbCatalog::Climb 
+  
+  def self.boulder
+    # @boulder = ClimbCatalog::Climb.boulder #=> lists bouldering routes
+  end
+  
+  def self.sport
+    # @sport = ClimbCatalog::Climb.sport #=> lists sport climbing routes
+  end
+  
+  def self.trad
+    # @trad = ClimbCatalog::Climb.trad #=> lists trad climbing routes
+  end
+    
+  def grade
+    # @boulder_grade = ClimbCatalog::Climb.boulder.grade #=> lists bouldering routes by difficulty
+    # @sport_grade = ClimbCatalog::Climb.sport.grade #=> lists sport climbing routes by difficulty
+    # @trad_grade = ClimbCatalog::Climb.trad.grade #=> lists trad climbing routes by difficulty
+  end
+    
+    # summary and url methods?
+  
+end
