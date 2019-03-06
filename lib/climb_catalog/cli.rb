31da9058a281dcd8e5d43d82573149d541964a1b
Kelsey Low <kklowlow@gmail.com>
2019-03-06 01:29:44 +0000

    modified cli

--- lib/climb_catalog/cli.rb
+++ lib/climb_catalog/cli.rb
@@ -2,6 +2,10 @@ class ClimbCatalog::CLI
   
   def call
     puts "Climbing?"
+    list_categories
   end
   
+  def list_categories
+    puts "Bouldering, sport climbing, or traditional climbing?"
+  end
 end
