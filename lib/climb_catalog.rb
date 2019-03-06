51890239af38af040883593bf93ab37f5eb77f63
Kelsey Low <kklowlow@gmail.com>
2019-03-06 01:16:11 +0000

    updated bin, environment, and cli to run

--- lib/climb_catalog.rb
+++ lib/climb_catalog.rb
@@ -1,6 +1,9 @@
 require_relative "./climb_catalog/version.rb"
+require_relative "./climb_catalog/cli.rb"
 
-module ClimbCatalog
-  class Error < StandardError; end
+
+
+# module ClimbCatalog
+#  class Error < StandardError; end
   # Your code goes here...
-end
+# end
