94336b9099770951a7ea08a48abc080a8e29a1a2
Kelsey Low <kklowlow@gmail.com>
2019-03-08 22:49:51 +0000

    more updates to cli

--- lib/climb_catalog/cli.rb
+++ lib/climb_catalog/cli.rb
@@ -8,17 +8,19 @@ class ClimbCatalog::CLI
   def menu
     puts "Which U.S. state will you be climbing in?"
     input = gets.strip.downcase
+    # program goes to correct state
     
     puts "Are you interested in boulder, sport, or trad climbs?"
     input = gets.strip.downcase
-    
-    print_climbs(input)
+    # program collects correct type of climb
+    print_climbs(input) # program lists climbs of type
     
     puts "Which climb would you like beta for?"
     input = gets.strip.downcase
-    
+    # program goes to correct climb
     climb = ClimbCatalog::Climb.find(input.to_i) # should this be to_i in my case?
-    print_climb(climb)
+    
+    print_climb(climb) # program returns details for climb
     
     puts "Would you like to choose another climb? yes / no"
      input = gets.strip.downcase
@@ -30,6 +32,17 @@ class ClimbCatalog::CLI
       puts "Sorry, I don't understand. Please enter yes, no, or exit."
       menu
     end
+  end
+  
+  def print_climbs(type) # how do I pass in the type??
+    puts "#{type} climbs in #{scrape_location}:"
+    puts ""
+    ClimbCatalog::Climb.all.each do |climb, index|
+      puts "#{index}. #{climb.name}, #{climb.location}"
+  end
+  
+  def print_climb(climb)
+    
   end
   
 end
