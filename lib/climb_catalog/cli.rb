c27b9f3cf803fe332bb859ec9163e444625c741a
Kelsey Low <kklowlow@gmail.com>
2019-03-08 23:18:25 +0000

    updating cli

--- lib/climb_catalog/cli.rb
+++ lib/climb_catalog/cli.rb
@@ -13,14 +13,27 @@ class ClimbCatalog::CLI
     puts "Are you interested in boulder, sport, or trad climbs?"
     input = gets.strip.downcase
     # program collects correct type of climb
-    print_climbs(input) # program lists climbs of type
+    # print_climbs(input) # program lists climbs of type
+    if input == "boulder"
+      puts "boulder1, boulder2, boulder3"
+    elsif input == "sport"
+      puts "sport1, sport2, sport3"
+    elsif input == "trad"
+      puts "trad1, trad2, trad3"
+    else
+      puts "Sorry, I don't understand. Please enter a type of climb or exit."
+    end
     
     puts "Which climb would you like beta for?"
     input = gets.strip.downcase
     # program goes to correct climb
-    climb = ClimbCatalog::Climb.find(input.to_i) # should this be to_i in my case?
-    
-    print_climb(climb) # program returns details for climb
+    #climb = ClimbCatalog::Climb.find(input.to_i) # should this be to_i in my case?
+    if input != nil
+      puts "aaaaaaaalllllllllllllllll the info on your climb!"
+    else
+      puts "Sorry, which climb?"
+    end
+    #print_climb(climb) # program returns details for climb
     
     puts "Would you like to choose another climb? yes / no"
      input = gets.strip.downcase
@@ -39,6 +52,7 @@ class ClimbCatalog::CLI
     puts ""
     ClimbCatalog::Climb.all.each do |climb, index|
       puts "#{index}. #{climb.name}, #{climb.location}"
+    end
   end
   
   def print_climb(climb)
