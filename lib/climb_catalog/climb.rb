ace47be7e2741cfac498d280ec43af4d0c933451
Kelsey Low <kklowlow@gmail.com>
2019-03-09 21:36:32 +0000

    updating scraper

--- lib/climb_catalog/climb.rb
+++ lib/climb_catalog/climb.rb
@@ -5,20 +5,9 @@ class ClimbCatalog::Climb
   @@all = []
   
   def self.new_from_classic_climbs(url)
-    #self.new(
-      #route.css("td").attribute("href").text, # name
-      #route.css(".small .text-warm .pl-half").text, # type
-      #route.css(".rateYDS").text, # grade
-      #route.css(".text-warm").text # location
-      #)
-      
     climb = Climb.new
-    # fill in climb's data
-    doc = Nokogiri::HTML(open(url))
-    name = doc.search("tr.route-row").attribute("href").text
-    type = doc.search("span.small .text-warm .pl-half").text
-    grade = doc.search("span.rateYDS").text
-    location = doc.search("span.text-warm").attribute("href").text
+    
+    properties = Scraper.new
     
     climb
   end
--- lib/climb_catalog/scraper.rb
+++ lib/climb_catalog/scraper.rb
@@ -1,5 +1,16 @@
 class ClimbCatalog::Scraper
   
+  def initialize(url)
+      # fill in climb's data
+    doc = Nokogiri::HTML(open(url))
+    climb = {}
+    
+    climb[:name] = doc.search("tr.route-row").attribute("href").text
+    climb.[:type] = doc.search("span.small .text-warm .pl-half").text
+    climb.[:grade] = doc.search("span.rateYDS").text
+    climb.[:location] = doc.search("span.text-warm").attribute("href").text
+  end
+    
   def get_page
     Nokogiri::HTML(open("https://www.mountainproject.com/area/classics/105800315/fort-collins"))
   end
@@ -10,7 +21,7 @@ class ClimbCatalog::Scraper
 
   def create_climbs
     scrape_classic_climbs.each do |route|
-      ClimbCatalog::Climb.new_from_classic_climbs(route)
+      ClimbCatalog::Climb.new_from_classic_climbs(url)
     end
   end
   
