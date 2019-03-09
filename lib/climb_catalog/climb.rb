159cbb42f12c1f75cd84c9fb7f7277d1c1b70623
Kelsey Low <kklowlow@gmail.com>
2019-03-09 00:44:04 +0000

    adding to climb.rb

--- lib/climb_catalog/climb.rb
+++ lib/climb_catalog/climb.rb
@@ -1,19 +1,24 @@
 class ClimbCatalog::Climb 
   
-  attr_accessor :name, :type, :grade, :location, :description, :url
+  attr_accessor :name, :type, :grade, :state, :location, :description, :url
   
   @@all = []
   
-  def self.new_from_classics_page(x)
-    self.new()
+  def self.scrape_classics_page(route)
+    self.new(
+      # name - route.css("td").attribute("href").text
+      # type - route.css(".small .text-warm .pl-half").text
+      # grade - route.css(".rateYDS").text
+      # state - route.css(".left-nav-row .l-2").attribute("href").text
+      # location - route.css(".text-warm").text
+      )
   end
   
-  def initialize(name=nil, type=nil, grade=nil, location=nil, url=nil)
-    @name = name
+  def initialize(name=nil, type=nil, grade=nil, location=nil)
+    @name = name 
     @type = type
     @grade = grade
     @location = location
-    @url = url
     @@all << self
   end
   
