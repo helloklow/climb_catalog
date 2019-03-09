9b883321f81b414476b802e5f6fedbda058903b6
Kelsey Low <kklowlow@gmail.com>
2019-03-09 23:52:39 +0000

    still troubleshooting scraper

--- lib/climb_catalog/cli.rb
+++ lib/climb_catalog/cli.rb
@@ -10,7 +10,7 @@ class ClimbCatalog::CLI
     puts "What type of climb are you interested in? Please enter boulder, sport, or trad."
     input = gets.strip.downcase
     
-    print_climbs(input) # program lists climbs of type
+    ClimbCatalog::Scraper.print_climbs(input) # program lists climbs of type
   
     choose_climb
   end
@@ -49,41 +49,7 @@ class ClimbCatalog::CLI
   
   
   
-  def get_page
-    doc = Nokogiri::HTML(open("https://www.mountainproject.com/area/classics/105800315/fort-collins"))
-  end
-  
-  def get_climbs
-    #climb = {}
-    #climb[:name] = doc.search("tr.route-row").attribute("href").text
-    #climb[:type] = doc.search("span.small .text-warm .pl-half").text
-    #climb[:grade] = doc.search("span.rateYDS").text
-    #climb[:location] = doc.search("span.text-warm").attribute("href").text
-    #climb
-    
-    self.get_page.css("tr.route-row a")
-  end
-  
-  def make_climbs
-    self.get_climbs.each do |route|
-      route = Climb.new
-      route.name = route.css("tr.route-row a").text
-      route.type = route.css("span.small .text-warm .pl-half").text
-      route.grade = route.css("span.rateYDS").text
-      route.location = route.css("span.text-warm a").text
-    end
-  end
-  
-  def print_climbs(input)
-    self.make_climbs
-    Climb.all.select do |route|
-      if route.type == input
-        puts "#{route.type.capitalize} climbs in Fort Collins:"
-        puts ""
-        puts "#{index}. #{route.name} - #{route.grade}, #{route.location}"
-      end
-    end
-  end
+
   
   #def print_climbs(type) # how do I pass in the type??
     #puts "#{type.capitalize} climbs in Fort Collins:"
--- lib/climb_catalog/scraper.rb
+++ lib/climb_catalog/scraper.rb
@@ -4,6 +4,37 @@ class ClimbCatalog::Scraper
     doc = Nokogiri::HTML(open("https://www.mountainproject.com/area/classics/105800315/fort-collins"))
   end
   
+  def get_climbs
+    #climb = {}
+    #climb[:name] = doc.search("tr.route-row").attribute("href").text
+    #climb[:type] = doc.search("span.small .text-warm .pl-half").text
+    #climb[:grade] = doc.search("span.rateYDS").text
+    #climb[:location] = doc.search("span.text-warm").attribute("href").text
+    #climb
+    
+    self.get_page.css("tr.route-row a")
+  end
+  
+  def make_climbs
+    self.get_climbs.each do |route|
+      route = Climb.new
+      route.name = route.css("tr.route-row a").text
+      route.type = route.css("span.small .text-warm .pl-half").text
+      route.grade = route.css("span.rateYDS").text
+      route.location = route.css("span.text-warm a").text
+    end
+  end
+  
+  def print_climbs(input)
+    self.make_climbs
+    Climb.all.select do |route|
+      if route.type == input
+        puts "#{route.type.capitalize} climbs in Fort Collins:"
+        puts ""
+        puts "#{index}. #{route.name} - #{route.grade}, #{route.location}"
+      end
+    end
+  end
 
   
 
