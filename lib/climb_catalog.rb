2b7c87fcffcdc1f2765c8552ba24fe9f92f39b9c
Kelsey Low <kklowlow@gmail.com>
2019-03-09 00:50:00 +0000

    updated climb_catalog.rb to require pry, nokogiri, and open-uri

--- lib/climb_catalog.rb
+++ lib/climb_catalog.rb
@@ -1,3 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 require_relative "./climb_catalog/version.rb"
 require_relative "./climb_catalog/cli.rb"
 require_relative "./climb_catalog/climb.rb"
