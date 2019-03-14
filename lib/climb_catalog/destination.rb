3780dd7f3c864c189909afe40cfc13a71445cf10
Kelsey Low <kklowlow@gmail.com>
2019-03-14 03:45:57 +0000

    updating destination for new pattern

--- lib/climb_catalog/destination.rb
+++ lib/climb_catalog/destination.rb
@@ -4,35 +4,40 @@ class ClimbCatalog::Destination
   
   @@all = []
   
-  def self.new_from_web(location)
-    self.new(
-      location.css("h3").text.strip, # h3 provides name attr
-      location.css("h2").text.strip, # h2 provides type attr
-      location.css("p").text.strip # p provides description attr
-      )
-  end
+ # def self.new_from_web(location)
+   # self.new(
+     # location.css("h3").text.strip, # h3 provides name attr
+     # location.css("h2").text.strip, # h2 provides type attr
+     # location.css("p").text.strip # p provides description attr
+     # )
+  #end
   
-  def initialize(name, type, description)
-    @name = name
-    @type = type
-    @description = description
-    save
-  end
-  
-  def save
+  def initialize#(name, type, description)
+    #@name = name
+    #@type = type
+    #@description = description
+    #save
     @@all << self
   end
   
-  def self.all
-    @@all
+  def list_by_type
+    self.all.select {|type| type == destination.type}
   end
   
-  def self.find_by_name(name)
-   self.all[name]
-  end
+  #def save
+   # @@all << self
+  #end
   
-  def self.find_by_type(type)
-   self.all[type]
-  end
+ # def self.all
+   # @@all
+  #end
+  
+  #def self.find_by_name(name)
+   #self.all[name]
+ # end
+  
+ # def self.find_by_type(type)
+  # self.all[type]
+  #end
     
 end
