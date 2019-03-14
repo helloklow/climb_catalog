2bb8ab57d559f5280723b778b84ffca7b9d91ba1
Kelsey Low <kklowlow@gmail.com>
2019-03-14 22:44:28 +0000

    changes

--- lib/climb_catalog/cli.rb
+++ lib/climb_catalog/cli.rb
@@ -11,6 +11,9 @@ class ClimbCatalog::CLI
       puts ""
       puts "We've found 15 of the most iconic worldwide climbing destinations:"
       @destinations = ClimbCatalog::Destination.all_destinations
+      @destinations.each do |destination|
+        puts "#{destination.name} - #{destination.type}"
+      end
   end
    
   def menu
@@ -19,18 +22,13 @@ class ClimbCatalog::CLI
       puts "Enter the number of the destination that you would like to learn about:"
       puts "Enter 'all' to see all destinations again, or 'exit' to leave."
       input = gets.strip
-      case input
-      when "1"
-        puts "Chaltén Massif, Argentina description"
-        puts ""
-      when "2"
-        puts "Mount Arapiles, Australia description"
-        puts ""
-      when "3"
-        puts "Bugaboos, Canada description"
-        puts ""
-      when "all"
+      
+      if input.to_i > 0 # if input is an integer and not a string 
+        puts @destinations[input.to_i-1] # puts the destination at the correct index
+      elsif input == "all"
         list_destinations
+      else
+        puts "I'm sorry, I don't understand."
       end
     end
   end
