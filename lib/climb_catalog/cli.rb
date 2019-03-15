ed9863281f9a8843f05bc7ba66451eecf06bc25a
Kelsey Low <kklowlow@gmail.com>
2019-03-15 04:04:41 +0000

    updating cli

--- lib/climb_catalog/cli.rb
+++ lib/climb_catalog/cli.rb
@@ -1,13 +1,76 @@
 class ClimbCatalog::CLI
 
   def call
-    ClimbCatalog::Scraper.new.scrape_destinations
-    puts "Welcome to the Top International Climbing Destinations!"
+    start
     puts ""
     menu
   end
 
+  def start
+    puts ""
+    puts "Welcome to the Top International Climbing Destinations!"
+    puts ""
+    puts "We've located 15 of the most iconic worldwide climbing destinations!"
+    puts "+ Enter 'list' to view all destinations."
+    #puts "+ Enter the destination number to learn more."
+    puts "+ Enter 'exit' to leave."
+    puts ""
+  end
+  
   def menu
+    input = nil 
+    puts "What would you like to do?"
+    input = gets.strip.downcase
+      case input
+      when "list"
+        puts list_destinations
+        puts "Enter the number of the destination that you would like to learn more about:"
+        answer = gets.strip.to_i
+        print_destination(answer)
+      when "exit"
+        goodbye
+      end
+  end
+  
+  def list_destinations
+    ClimbCatalog::Scraper.get_destinations
+    ClimbCatalog::Scraper.create_destinations
+    ClimbCatalog::Destination.all.each.with_index do |d, i|
+      puts "#{d.name}"
+    end
+  end
+  
+  def print_destination(answer)
+    ClimbCatalog::Destination.all.each.with_index do |d, i|
+      if i == answer
+        puts "#{d.name} - #{d.type}"
+        puts "#{d.description}"
+      end
+    end
+  end
+  
+  def goodbye
+    puts "Climb on!"
+    exit
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin 
+ def menu
     puts "We've found 15 of the most iconic worldwide climbing destinations:"
     list_destinations
     
@@ -50,5 +113,4 @@ class ClimbCatalog::CLI
     puts "#{destination.description}"
     puts ""
   end
-
-end
+=end
