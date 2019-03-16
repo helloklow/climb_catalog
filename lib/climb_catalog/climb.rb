8e4ebdba2c50e2bb888e4558d72fd17757a39001
Kelsey Low <kklowlow@gmail.com>
2019-03-16 19:12:26 +0000

    updating scraper with rows

--- lib/climb_catalog/climb.rb
+++ lib/climb_catalog/climb.rb
@@ -26,16 +26,17 @@ class ClimbCatalog::Climb
 
   def self.scrape_climbs
     doc = Nokogiri::HTML(open("https://www.mountainproject.com/area/classics/105800315/fort-collins"))
-    row = doc.css("div.table-responsive table.table.route-table.hidden-xs-down")
-
-    name = doc.css("td a strong").text.strip
-    location = doc.css("td span.small span.text-warm a").text.strip
-    rating = doc.css("td span.rateYDS").text.strip
-    type = doc.css("td span.small.text-warm.pl-half span").text.strip
-    url = doc.css("td href").text.strip
-      
-    climb = self.new(name, location, rating, type, url)
-    climb.save
+    row = doc.css("div.table-responsive table.table.route-table.hidden-xs-down tr.route-row")
+    
+    row.each do |c|
+      name = c.css("td a strong").text.strip
+      location = c.css("td span.small span.text-warm a").text.strip
+      rating = c.css("td span.rateYDS").text.strip
+      type = c.css("td span.small.text-warm.pl-half span").text.strip
+      url = c.css("td href").text.strip
+      climb = self.new(name, location, rating, type, url)
+      climb.save
+    end
   end
   
 end
